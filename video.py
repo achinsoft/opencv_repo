fab4195c22c6632cb1658bfc49ead7bf4daedc45
Achinsoft <shaikh.raja@outlook.com>
2019-11-25 20:21:53 +0530

    initial commit

--- video.py
+++ video.py
@@ -7,9 +7,11 @@ while True:
     check, frame = video.read()
     print(check)
     print(frame)
-
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
     cv2.imshow("img", frame)
-    key=cv2.waitKey(1)
+    cv2.imshow("gray", gray)
+    key = cv2.waitKey(1)
 
     if key == ord('q'):
         break
