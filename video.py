fa059c5171a837f1050ff4c304b1c8e1f9435483
Achinsoft <shaikh.raja@outlook.com>
2019-11-26 17:32:09 +0530

    initial commit

--- video.py
+++ video.py
@@ -5,13 +5,16 @@ video = cv2.VideoCapture(0)
 while True:
 
     check, frame = video.read()
+
+    frame=cv2.resize(frame, (200, 200))
     print(check)
     print(frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    guss = cv2.GaussianBlur(gray, (21, 21), 0)
     cv2.imshow("img", frame)
     cv2.imshow("gray", gray)
-    key = cv2.waitKey(1)
+    cv2.imshow("guss", guss)
+    key = cv2.waitKey(100)
 
     if key == ord('q'):
         break
